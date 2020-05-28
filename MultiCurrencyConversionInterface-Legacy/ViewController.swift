6d13406798debaecd0b6f8c6d783cfb00d55d596
JohnKuan <founders2008@hotmail.com>
2020-05-28 10:21:58 +0800

    Initial Commit

---
+++ MultiCurrencyConversionInterface-Legacy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiCurrencyConversionInterface-Legacy
+//
+//  Created by John Kuan on 28/5/20.
+//  Copyright © 2020 JohnKuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
